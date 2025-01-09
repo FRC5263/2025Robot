8a1af86201bfae9becbde8c4ed5ca234400e09b6
CookieDev <msettaher@gmail.com>
2025-01-09 15:46:53 -0500

    AHHHHHHHHHHHH

---
+++ src/main/java/frc/robot/subsystems/Pneumatics.java
@@ -0,0 +1,22 @@
+package frc.robot.subsystems;
+
+import frc.robot.commands.TeleOp;
+import edu.wpi.first.wpilibj.Compressor;
+import edu.wpi.first.wpilibj.DoubleSolenoid;
+import edu.wpi.first.wpilibj.PneumaticsModuleType;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+
+public class Pneumatics extends SubsystemBase {
+    //both double solinoids (tubes)
+     private final static DoubleSolenoid m_doubleSolenoidBottomL =
+           new DoubleSolenoid(PneumaticsModuleType.REVPH, 1, 2);
+           private final DoubleSolenoid m_doubleSolenoidTopL =
+           new DoubleSolenoid(PneumaticsModuleType.REVPH, 3, 4);
+               private final Compressor m_compressor = new Compressor(PneumaticsModuleType.REVPH);
+     
+
+            
+
+    
+}
