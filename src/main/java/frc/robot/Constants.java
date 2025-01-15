9a9a90224d4a3d392ed87335a9649412c1f43695
tthehuman <miket5279@gmail.com>
2025-01-15 09:30:29 -0800

    quick CAN ID change

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -9,10 +9,11 @@ public final class Constants {
    * Motor order:
    * First 4 ALWAYS drivetrain
    * More to come
+   * There's no 0-indexing on the CAN IDs >:(
    */
   public static final int CANID[] = {0, 1, 2, 3};
-  public static final int frontRightID = CANID[0];
-  public static final int backRightID = CANID[1];
-  public static final int backLeftID = CANID[2];
-  public static final int frontLeftID = CANID[3];
+  public static final int frontRightID = CANID[1];
+  public static final int backRightID = CANID[2];
+  public static final int backLeftID = CANID[3];
+  public static final int frontLeftID = CANID[4];
 }
