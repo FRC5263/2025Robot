2367304e7c76d13e41c0b0d733eb3fd0fea56730
tthehuman <miket5279@gmail.com>
2025-01-15 11:22:59 -0800

    nice work malloc();

--- src/main/java/frc/robot/subsystems/Pneumatics.java
+++ src/main/java/frc/robot/subsystems/Pneumatics.java
@@ -1,7 +1,6 @@
 //Important to the change of the code: https://docs.wpilib.org/en/stable/docs/hardware/hardware-basics/wiring-pneumatics-ph.html (mostly for the pressure sensor.)
 package frc.robot.subsystems;
 
-import frc.robot.commands.TeleOp;
 import edu.wpi.first.wpilibj.Compressor;
 import edu.wpi.first.wpilibj.DoubleSolenoid;
 import edu.wpi.first.wpilibj.PneumaticsModuleType;
