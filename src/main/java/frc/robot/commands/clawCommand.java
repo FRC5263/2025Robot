24646a6201405d012053caec972f6739ff908b8e
Cookie <msettaher@gmail.com>
2025-03-06 17:31:15 -0500

    add methods, TeleOp done but not commit

--- src/main/java/frc/robot/commands/clawCommand.java
+++ src/main/java/frc/robot/commands/clawCommand.java
@@ -3,10 +3,10 @@ package frc.robot.commands;
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.subsystems.claw;
 public class clawCommand extends Command {
-    private claw m_claw;
+    private claw m_Claw;
         public clawCommand(claw clawSubsystem){
-            m_claw = clawSubsystem;
-            addRequirements(m_claw);
+            m_Claw = clawSubsystem;
+            addRequirements(m_Claw);
         }
 
         @Override
@@ -20,8 +20,8 @@ public class clawCommand extends Command {
   @Override
   public void end(boolean interrupted) {
     claw.Wrist.set(0);
-    claw.intake1.set(0);
-    claw.intake2.set(0);
+    claw.intake_coral.set(0);
+    claw.intake_alg.set(0);
   }
 
   @Override
--- src/main/java/frc/robot/subsystems/claw.java
+++ src/main/java/frc/robot/subsystems/claw.java
@@ -1,21 +1,57 @@
 package frc.robot.subsystems;
 
-
-import com.revrobotics.spark.SparkMax;
-import com.revrobotics.spark.SparkLowLevel.MotorType;
-
-import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
+import com.revrobotics.spark.SparkLowLevel.MotorType;
+import com.revrobotics.spark.config.SparkMaxConfig;
+import com.revrobotics.spark.SparkMax;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class claw extends SubsystemBase {
     public static SparkMax Wrist;
-    public static SparkMax intake1;
-    public static SparkMax intake2;
+    public static SparkMax intake_coral;
+    public static SparkMax intake_alg;
+    private SparkMaxConfig invertConfig;
+    private SparkMaxConfig notinvertConfig;
     
     public claw(){
-    //mota
+    
         Wrist = new SparkMax(Constants.WristID, MotorType.kBrushless);
-        intake1 = new SparkMax(Constants.intake1_ID, MotorType.kBrushless);
-        intake2 = new SparkMax(Constants.intake2_ID, MotorType.kBrushless);
+        intake_coral = new SparkMax(Constants.intake1_ID, MotorType.kBrushless);
+        intake_alg = new SparkMax(Constants.intake2_ID, MotorType.kBrushless);
+        invertConfig = new SparkMaxConfig();
+        notinvertConfig = new SparkMaxConfig();
+
+    invertConfig.inverted(true);
+        notinvertConfig.inverted(false);
+        
+ 
+
     }
+    // this method sucks terribly, but again its java.
+   /* (i don't think this is gonna work by logic) public void invert(){
+        Wrist.configure(invertConfig, null, null);
+        intake_alg.configure(invertConfig, null, null);
+        intake_coral.configure(invertConfig, null, null);
+    }
+        */
+    public void Wristinvert(){
+        Wrist.configure(invertConfig, null, null);
+    }
+    public void Alginvert(){
+        intake_alg.configure(invertConfig, null, null);
+    }
+    public void Coralinvert(){
+        intake_coral.configure(invertConfig, null, null);
+    }
+    public void deWristinvert(){
+        Wrist.configure(notinvertConfig, null, null);
+    }
+    public void deAlginvert(){
+        intake_alg.configure(notinvertConfig, null, null);
+    }
+    public void deCoralinvert(){
+        intake_coral.configure(notinvertConfig, null, null);
+    }
+
 }
+
